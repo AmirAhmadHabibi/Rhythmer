d414355521f199c2ed5f16b067f932219c9279dc
Amirahmadhabibi <amirahmad.habibi@gmail.com>
2018-08-15 00:08:15 -0400

    Dehkhoda and Moein pronunciation extraction for basic cases + minor fixes in tanveen and tashdeed + added some comments

--- wordie.py
+++ wordie.py
@@ -18,65 +18,73 @@ class Wordie:
 
     def add_pronunciation(self, prn, dictionary):
         if dictionary == 'dehkhoda':
-            # extract the consonent vowel pairs out of the raw formula
+            # extract the consonant vowel pairs out of the raw formula
             prn_list = [[]]
             list_index = 0  # in case of seeng / this index would be added
-            consonent = None
+            consonant = None
             prn = prn.replace('\/', '-')
             prn = prn.replace('ً', 'َ')
             for c in prn:
                 if c == ' ':
                     continue
-                elif consonent is None and c in letters:
-                    consonent = c
+                elif consonant is None and c in letters:
+                    consonant = c
                 elif c in short_vowels or c in long_vowels:
                     vowel = c
-                    if consonent is None:
+                    if consonant is None:
                         raise Exception('What is this vowel doing here? ' + self.text + '--' + prn + '--' + c)
-                    prn_list[list_index].append((consonent, vowel))
-                    consonent = None
+                    prn_list[list_index].append((consonant, vowel))
+                    consonant = None
                 elif c == '-':
                     list_index += 1
                     prn_list.append([])
-                    consonent = None
+                    consonant = None
                 elif c in letters:
-                    prn_list[list_index].append((consonent, 'ْ'))
-                    consonent = c
+                    prn_list[list_index].append((consonant, 'ْ'))
+                    consonant = c
                 else:
                     raise Exception('What is this char? ' + self.text + '--' + prn + '--' + c)
-            if consonent is not None:
-                prn_list[list_index].append((consonent, 'ْ'))
+            if consonant is not None:
+                prn_list[list_index].append((consonant, 'ْ'))
 
-            # create text with vowels out of the extracted pairs of consonent vowels
+            # create text with vowels out of the extracted pairs of consonant vowels
             for pair_list in prn_list:
                 prn_word = ''
                 pair_index = 0
                 for i in range(len(self.text)):
+                    # if there is a cons-vow pair for this character
                     if pair_index < len(pair_list) and self.text[i] == pair_list[pair_index][0]:
+                        # the case of semi tashdeeds on ی
                         if pair_index + 1 < len(pair_list) and \
                                 pair_list[pair_index][1] == 'ی' and pair_list[pair_index + 1][0] == 'ی':
                             prn_word += pair_list[pair_index][0] + pair_list[pair_index][1]
-                        elif pair_list[pair_index][1] != 'ْ':
-                            if pair_list[pair_index][1] not in long_vowels:
-                                prn_word += pair_list[pair_index][0] + pair_list[pair_index][1]
-                            else:
-                                prn_word += pair_list[pair_index][0]
-                        elif pair_index + 1 < len(pair_list) and \
-                                pair_list[pair_index][0] == pair_list[pair_index + 1][0]:  # the case of Tashdeed I hope
-                            prn_word += pair_list[pair_index][0]
-                            if pair_list[pair_index + 1][1] not in long_vowels:
-                                prn_word += pair_list[pair_index + 1][0] + pair_list[pair_index + 1][1]
-                            else:
+                        # the case of Tashdeed I hope
+                        elif pair_list[pair_index][1] == 'ْ' and pair_index + 1 < len(pair_list) and \
+                                pair_list[pair_index][0] == pair_list[pair_index + 1][0]:
+                            # add the consonant with Saken first
+                            prn_word += pair_list[pair_index][0] + pair_list[pair_index][1]
+                            # then add the second consonant with vowel
+                            if pair_list[pair_index + 1][1] in long_vowels:
                                 prn_word += pair_list[pair_index + 1][0]
+                            else:
+                                prn_word += pair_list[pair_index + 1][0] + pair_list[pair_index + 1][1]
                             pair_index += 1
-                        else:
+                        # the case of having long vowels in phonetics
+                        elif pair_list[pair_index][1] in long_vowels:
                             prn_word += pair_list[pair_index][0]
+                        # other normal cases
+                        else:
+                            prn_word += pair_list[pair_index][0] + pair_list[pair_index][1]
                         pair_index += 1
+                    # if there are no cons-vow pairs for this character
                     else:
                         prn_word += self.text[i]
+                
                 prn_word = prn_word.replace('ْ', '')
+                # add the voweled word to the set of all pronunciations
                 self.text_vowel.add(prn_word)
                 print(dictionary, ':\t', prn, '|', prn_word, '|')
+
         elif dictionary == 'moein':
             # TODO
             pass
