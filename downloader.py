56ad02cb1628f0a1dd98f0ace4c988b3925ebd5e
Amirahmadhabibi <amirahmad.habibi@gmail.com>
2018-08-14 22:01:37 -0400

    Dehkhoda and Moein pronunciation extraction for basic cases

--- downloader.py
+++ downloader.py
@@ -1,45 +1,116 @@
 import requests
-
-letters = 'آابپتثجچحخدذرزژسشصضطضعغفقکگلمنوهیيئءؤأإةۀ'
-short_vowels = 'َُِْ'
-long_vowels = 'ایيو'
+from wordie import Wordie
 
 
-# TODO check tanveens
-
-
-def send_request(word='سلام'):
+def send_request(word='سلام', dictionaries='dehkhoda'):
     try:
+        # TODO: verbs and compounds
+        print('-------')
+        print(word)
         response = requests.get(
             url="http://api.vajehyab.com/v3/search",
             params={
                 "token": "26950.1yq8LLtXqUIfJj070qK6qQtWVe50ioikWIsItVFU",
                 "q": word,
                 "type": "exact",
-                "filter": "dehkhoda,moein,amid",
+                "filter": dictionaries,
             },
         )
-        print('Response HTTP Status Code: {status_code}'.format(status_code=response.status_code))
-        print('Response HTTP Response Body: {content}'.format(content=response.content))
+
+        # "filter": "dehkhoda,moein,amid",
+        # print('Response HTTP Status Code: {status_code}'.format(status_code=response.status_code))
+        # print('Response HTTP Response Body: {content}'.format(content=response.content))
         resp = str(response.content, encoding='utf-8')
         resp = resp.replace('true', 'True')
         resp = resp.replace('null', 'None')
         resp = eval(resp)
+        wordie = Wordie(word)
         for res in resp['data']['results']:
             if res['id'].startswith('dehkhoda'):
-                phon = res['text'].partition('[')[2].partition(']')[0]
-                phon = ''.join(phon.split())
-                # TODO
+                pron = res['text'].partition('[')[2].partition(']')[0]
+                pron = ''.join(pron.split())
+                try:
+                    wordie.add_pronunciation(pron, 'dehkhoda')
+                except Exception as e:
+                    print(e)
             elif res['id'].startswith('moein'):
-                phon = res['text'].partition('(')[2].partition(')')[0]
-                # TODO
-                pass
+                pron = res['text'].partition('(')[2].partition(')')[0]
+                try:
+                    wordie.add_pronunciation(pron, 'moein')
+                except Exception as e:
+                    print(e)
             elif res['id'].startswith('amid'):
-                phon=res['pron']
-                # TODO
-                pass
+                try:
+                    wordie.add_pronunciation(res['pron'], 'amid')
+                except Exception as e:
+                    print(e)
+        return wordie
     except requests.exceptions.RequestException:
         print('HTTP Request failed')
 
 
-send_request()
+send_request(word='کتف', dictionaries='moein,amid')
+send_request(word='کتف', dictionaries='dehkhoda')
+send_request(word='دروازه بان', dictionaries='moein,amid')
+send_request(word='دروازه بان', dictionaries='dehkhoda')
+send_request(word='قطعاً', dictionaries='moein,amid')
+send_request(word='قطعاً', dictionaries='dehkhoda')
+send_request(word='کرک', dictionaries='moein,amid')
+send_request(word='کرک', dictionaries='dehkhoda')
+send_request(word='ابر', dictionaries='moein,amid')
+send_request(word='ابر', dictionaries='dehkhoda')
+
+# -------
+# کتف
+# -------
+# dehkhoda کَ
+# dehkhoda کَتَ
+# dehkhoda کَتَ
+# dehkhoda کَتِ\/کِ\/کَ\/کَتَ
+# dehkhoda کِ
+# dehkhoda کُ
+# moein کِ
+# amid ketf
+# -------
+# دروازه بان
+# -------
+# dehkhoda دَرْزَ\/زِ
+# amid darvāzebān
+# -------
+# قطعاً
+# -------
+# dehkhoda قَعَنْ
+# moein قَ عَ نْ
+# -------
+# کرک
+# -------
+# dehkhoda
+# dehkhoda
+# dehkhoda
+# dehkhoda
+# dehkhoda کَ
+# dehkhoda کَ
+# dehkhoda کَ
+# dehkhoda کَ
+# dehkhoda کَ
+# dehkhoda کَرَ
+# -------
+# ابر
+# -------
+# moein   اَ
+# moein اَ بَ
+# moein  ~.
+# moein  ~.
+# amid &#39;abr
+# amid &#39;abar
+# -------
+# ابر
+# -------
+# dehkhoda اَ
+# dehkhoda اَ
+# dehkhoda اَبَ
+# dehkhoda اَبَرر
+# dehkhoda اِبَ
+# dehkhoda اُبُ
+# dehkhoda اُبُ
+# dehkhoda اَ
--- wordie.py
+++ wordie.py
@@ -1,18 +1,60 @@
 letters = 'آابپتثجچحخدذرزژسشصضطضعغفقکگلمنوهیيئءؤأإةۀ'
-short_vowels = 'َُِ'
 long_vowels = 'ایيو'
+short_vowels = 'َ ُ ِ ْ'
 
-# TODO check tanveens
 
+# َ ُ ِ ْ
+# tanveens
+# tashdeed
+# be and beh cases
+# first letter should have vowel
 
 class Wordie:
     def __init__(self, text):
         self.text = text
         self.pronunciations = set()
 
-    def add_pronunciation(self, prn):
-        # TODO
-        pass
+    def add_pronunciation(self, prn, dictionary):
+        if dictionary == 'dehkhoda' or dictionary == 'moein':
+            # extract the consonent vowel pairs out of the raw formula
+            prn_list = [[]]
+            list_index = 0  # in case of seeng / this index would be added
+            consonent = None
+            prn = prn.replace('\/', '-')
+            for c in prn:
+                if c == ' ':
+                    continue
+                elif c in letters:
+                    consonent = c
+                elif c in short_vowels:
+                    vowel = c
+                    if consonent is None:
+                        raise Exception('What is this vowel doing here? ' + self.text + '--' + prn + '--' + c)
+                    if vowel != 'ْ':
+                        prn_list[list_index].append((consonent, vowel))
+                    consonent = None
+                elif c == '-':
+                    list_index += 1
+                    prn_list.append([])
+                    consonent = None
+                else:
+                    raise Exception('What is this char? ' + self.text + '--' + prn + '--' + c)
+
+            # create pronunciations out of the extracted pairs
+            for pair_list in prn_list:
+                prn_word = ''
+                pair_index = 0
+                for i in range(len(self.text)):
+                    if pair_index < len(pair_list) and self.text[i] == pair_list[pair_index][0]:
+                        prn_word += pair_list[pair_index][0] + pair_list[pair_index][1]
+                        pair_index += 1
+                    else:
+                        prn_word += self.text[i]
+                self.pronunciations.add(prn_word)
+                print(dictionary, ':\t', prn, '|', prn_word, '|')
+        elif dictionary == 'amid':
+            # TODO
+            pass
 
 
 class Heja:
